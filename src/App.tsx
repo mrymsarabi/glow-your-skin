354e99430561e92e77bcfce7c67d8a9502113d1b
Maryam <mrymsarabi@gmail.com>
2025-04-17 21:22:49 +0330

    Navbar - setting the UI - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 // import React from 'react'
 import './App.css'
+import AboutUs from './sections/AboutUs'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+import Hero from './sections/Hero'
+import Navbar from './sections/Navbar'
 
 function App() {
   return (
     <div className={`bg-cream min-h-screen text-espresso`}>
-
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,8 +1,12 @@
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className="bg-cream text-espresso py-4 px-5 d-flex justify-content-between ">
+            <h1>Glow Your Skin</h1>
+            <ul className="d-flex list-unstyled">
+                <li><a href="#about" className="text-decoration-none mx-2">About</a></li>
+                <li><a href="#contact" className="text-decoration-none mx-2">Contact</a></li>
+            </ul>
+        </nav>
     );
 };
 

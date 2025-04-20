20460c4f629738eb53c270a8436dc418b4344f69
Maryam <mrymsarabi@gmail.com>
2025-04-20 20:55:52 +0330

    AboutUs - 1 - UI

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -1,7 +1,14 @@
 const AboutUs = () => {
     return (
-        <div>
-
+        <div className={`m-auto my-3 mt-5 w-75`}>
+            <h2 className={`text-center m-3`}>About Us</h2>
+            <div>
+                At Glow Your Skin, we believe that skincare is not just a routine — it’s a ritual of self-love. 
+                Our mission is to create simple, effective, and gentle skincare products 
+                that celebrate the natural beauty in every skin type. With a blend of clean ingredients, 
+                soothing formulations, and elegant design, we aim to bring a touch of calm and glow to your everyday 
+                skincare routine. Because glowing skin is always in.
+            </div>
         </div>
     );
 };
